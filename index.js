92521cb574c246fc9a561c76d35f2c46288f6c35
danainjax <danainjax@gmail.com>
2021-06-10 07:00:03 -0400

    map methods done, working on reduce

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Your code here
+
+// returns an array with all values made negative
+//          transforms correctly
+let newArray = []
+function mapToNegativize(array) {
+    array.forEach(element => {
+        
+        
+        newArray.push(element *-1)
+    });
+    return newArray
+}
+
+// mapToNoChange returns an array with the original values
+function mapToNoChange(array) {
+    return array
+}
+// mapToDouble returns an array with the original values multiplied by 2
+
+let doubles = []
+function mapToDouble(array) {
+    array.forEach(element=> {
+        doubles.push( element * 2) 
+    })
+    return doubles
+}
+
+// mapToSquare returns an array with the original values squared
+
+let squared = []
+function mapToSquare(array) {
+    array.forEach(element=> {
+        squared.push(element * element)
+    })
+    return squared
+}
